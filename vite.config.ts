01e61311120b9f9b02ec7355dc5fc5a5f3aa28dc
nkgrnkgr <beefupme@gmail.com>
2024-06-22 21:31:37 +0900

    update setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/remix-spa-mode/",
   plugins: [
     remix({
-      ssr: false,
+      basename: "/remix-spa-mode/",
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html"),
+        );
+      },
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
