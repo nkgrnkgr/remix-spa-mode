c5b1f95fd2b027c059931c84dd33da732fa88027
nkgrnkgr <beefupme@gmail.com>
2024-06-22 21:58:29 +0900

    update

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,6 +13,14 @@ export default function Index() {
     <div className="font-sans p-4">
       <h1 className="text-3xl">Welcome to Remix (SPA Mode)</h1>
       <ul className="list-disc mt-4 pl-6 space-y-2">
+        <li>
+          <Link
+            className="text-blue-700 underline visited:text-purple-900"
+            to="/about"
+          >
+            About
+          </Link>
+        </li>
         <li>
           <a
             className="text-blue-700 underline visited:text-purple-900"
--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -6,16 +6,18 @@ export const meta: MetaFunction = () => {
 
 export default function About() {
   return (
-    <div>
-      <div>
-        <h1>About</h1>
-        <p>This is the about page</p>
-        <p>
-          <Link className="text-sky-500 font-semibold dark:text-sky-400" to="/">
-            Go back home
+    <div className="font-sans p-4">
+      <h1 className="text-3xl">About</h1>
+      <ul className="list-disc mt-4 pl-6 space-y-2">
+        <li>
+          <Link
+            className="text-blue-700 underline visited:text-purple-900"
+            to="/"
+          >
+            Go Back Home
           </Link>
-        </p>
-      </div>
+        </li>
+      </ul>
     </div>
   );
 }
